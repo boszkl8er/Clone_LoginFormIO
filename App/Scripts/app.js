4631f2e75133279b176e0d5cf33b8b390a81cd7f
boszkl8er <kittipong.iti17@gmail.com>
2017-10-07 10:32:31 +0700

    d

--- App/Scripts/app.js
+++ App/Scripts/app.js
@@ -17,7 +17,8 @@ angular.module('catApp', ['ngRoute','AdalAngular'])
     adalProvider.init(
         {
             instance: 'https://login.microsoftonline.com/', 
-            tenant: 'together.at',
+            //tenant: 'together.at',
+			tenant: '3e0b6cbd-4959-4d01-81bf-ce883ddabd96',
 			//clientId: '0a07a77d-ee2e-477e-93ca-fd7f59421237', Old clientID
 			clientId: 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58',
             extraQueryParameter: 'nux=1',
